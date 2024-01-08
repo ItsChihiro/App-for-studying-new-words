0058195bc7ea2de38fd61d2d66b73cd86b78388b
Maryam <maryam@sosiv.io>
2024-01-08 23:52:06 +0200

    interactive card

---
+++ src/components/CardOfWord/CardOfWord.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './CardOfWord.scss'
+import MyButton from '../UI/button/MyButton';
+
+export default function WordCard(props) {
+    const { id, english, transcription, russian, tags } = props;
+
+    const [translation, setTranslation] = useState(false)
+    function handleTranlation() {
+        setTranslation(!translation)
+    }
+
+    return (
+        <div className="card">
+            <div className="card__content">
+                <h3 className="card__word">{english}</h3>
+                <p className="card__transcription">{transcription}</p>
+            </div>
+
+            <div className="answer">
+                {translation
+                    ? <p className="card__translation">{russian}</p>
+                    : <MyButton onClick={handleTranlation}>Check</MyButton>
+                }
+            </div>
+        </div>
+    );
+}
