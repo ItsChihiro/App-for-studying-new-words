6e3b31e32bf0e8f85a54a235b8812c66f4dca5fd
Maryam <maryam@sosiv.io>
2024-01-14 23:07:37 +0200

    App updates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,14 @@ import Footer from '../Footer/Footer'
 import cl from './App.module.css'
 import ListOfWords from '../TableWords/TableWords'
 import { words } from '../constants'
-import Cards from '../Cards/Cards'
+import Slider from '../Slider/Slider'
 
 export default function App() {
   return (
     <div className={cl.App}>
       <Header />
       <ListOfWords words={words} />
-      <Cards words={words}></Cards>
+      <Slider />
       <Footer />
     </div>
   )
