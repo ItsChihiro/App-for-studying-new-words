954082d5e2c7dcdd96a0def3a74177bcdb02b761
Maryam <maryam@sosiv.io>
2024-01-14 23:40:41 +0200

    remove comments

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -4,41 +4,31 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import style from './Slider.module.scss'
 import CardOfWord from '../CardOfWord/CardOfWord';
 
+//бесконечный слайдер
 export default function Slider() {
     const [wordIndex, setWordIndex] = useState(0);
     const [word, setWord] = useState(words[0])
 
-    const nextCard = () => {
+    const handleNextCard = () => {
         const index = (wordIndex + 1) % words.length;
         setWord(words[index]);
         setWordIndex(index);
-
-        // if (wordIndex < words.length - 1) {
-        //     setWordIndex(wordIndex + 1);
-        // } else {
-        //     //если достигнут конец массива, переход к первой карточке
-        //     setWordIndex(0);
-        // }
     };
 
-    const previousCard = () => {
+    const handlePreviousCard = () => {
         const index = wordIndex > 0 ? wordIndex - 1 : words.length - 1;
-        setWord(words[index])
-        setWordIndex(index)
-
-        // if (wordIndex > 0) {
-        //     setWordIndex(wordIndex - 1)
-        // } else {
-        //     //если достигнуто начало массива, переход к последней карточке
-        //     setWordIndex(words.length - 1)
-        // }
-    }
+        setWord(words[index]);
+        setWordIndex(index);
+    };
 
     return (
         <div className={style.container}>
-            <FaChevronLeft className={style.arrow} onClick={previousCard} />
-            <CardOfWord {...word} />
-            <FaChevronRight className={style.arrow} onClick={nextCard} />
+            <div className={style.slide}>
+                <FaChevronLeft className={style.arrow} onClick={handlePreviousCard} />
+                <CardOfWord {...word} />
+                <FaChevronRight className={style.arrow} onClick={handleNextCard} />
+            </div>
+            <div><div>{wordIndex + 1}/{words.length}</div></div>
         </div>
     );
 }
