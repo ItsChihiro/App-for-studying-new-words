897ecea8fdb2d9d047b82a50f09f16264160474d
Maryam <maryam@sosiv.io>
2024-02-01 16:53:18 +0200

    add game page

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './Game.module.css'
+import Slider from '../../components/Slider/Slider';
+import MyButton from '../../components/UI/button/MyButton';
 
-function Game(props) {
+export default function Game({ words }) {
+    const [start, setStart] = useState(false)
+    function handleClick() {
+        setStart(!start)
+    }
+
     return (
-        <div>
-
+        <div className={style.container}>
+            <h2>Word-learning simulator</h2>
+            {(!start)
+                ? <MyButton onClick={handleClick}>Start</MyButton>
+                : <Slider words={words} />
+            }
         </div>
     );
 }
 
-export default Game;
