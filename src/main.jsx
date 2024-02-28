a6cda9f24417744c8bfa458aad01230dc32e34d3
Maryam <maryam@sosiv.io>
2024-02-28 13:37:38 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import './style/index.scss'
+import { MyContextComponent } from './context/MyContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MyContextComponent>
+      <App />
+    </MyContextComponent>
+  </React.StrictMode>
 )
