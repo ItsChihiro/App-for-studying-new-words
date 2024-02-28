5b32d777abb6a4fd3c800464536dacbc489e3ab7
Maryam <maryam@sosiv.io>
2024-02-28 13:39:27 +0200

    add fetch api method

--- src/services/Get.js
+++ src/services/Get.js
@@ -1,10 +1,11 @@
 class Get {
     static async getWords() {
         try {
-            const resp = await fetch("/api/words");
+            const resp = await fetch("http://itgirlschool.justmakeit.ru/api/words");
             return await resp.json();
         } catch (e) {
             console.error(e)
+            return false
         }
     }
 }
