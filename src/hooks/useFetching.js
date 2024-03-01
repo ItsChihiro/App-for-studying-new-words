805302e1ffacc2848fd4f00c15a391003b8123e7
Maryam <maryam@sosiv.io>
2024-03-01 16:16:50 +0200

    update useFetching

--- src/hooks/useFetching.js
+++ src/hooks/useFetching.js
@@ -4,10 +4,10 @@ export const useFetching = (callback) => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
 
-    const fetching = async () => {
+    const fetching = async (...props) => {
         try {
             setIsLoading(true)
-            await callback()
+            await callback(...props)
         } catch (e) {
             setError(e.message)
             console.error(e)
