072d83b5ad761d8c3f61e9cfd14822c0838b5c7d
Maryam <maryam@sosiv.io>
2024-03-01 18:19:14 +0200

    update TableWords

--- src/components/TableWords/TableWords.jsx
+++ src/components/TableWords/TableWords.jsx
@@ -3,7 +3,7 @@ import './TableWords.scss'
 import { MyContext } from '../../context/MyContext';
 import { useContext } from 'react';
 import { useFetching } from '../../hooks/useFetching';
-import Get from '../../services/Api';
+import Api from '../../services/Api';
 import Loader from '../UI/Loader/Loader';
 
 export default function TableWords() {
@@ -11,8 +11,7 @@ export default function TableWords() {
 
     //функция удаления слова
     const [deleteWord, isDeleteLoading, deleteError] = useFetching(async (id) => {
-        console.log(id);
-        await Get.deleteWord(id);
+        await Api.deleteWord(id);
         const wordsServer = getWordsServer();
         setDataServer(wordsServer);
     })
@@ -20,21 +19,14 @@ export default function TableWords() {
 
     //функция обновления слова
     const [updateWord, isUpdateLoading, updateError] = useFetching(async (id, data) => {
-        const updatedData = await Get.updateWord(id, data)
+        const updatedWord = await Api.updateWord(id, data)
         setDataServer(dataServer.map((item) => {
             if (item.id === id) {
-                return { ...item, ...updatedData }
+                return { ...item, ...updatedWord }
             }
             return item;
         }))
     })
-
-    //функция добавления слова
-    const [addWord, isAddLoading, addError] = useFetching(async (id, data) => {
-        const newWord = await Get.addWord(id, data)
-        setDataServer([...dataServer, newWord])
-    })
-
 
 
     const rows = dataServer.map((row) => {
@@ -43,13 +35,12 @@ export default function TableWords() {
             key={row.id}
             deleteWord={deleteWord}
             updateWord={updateWord}
-            addWord={addWord}
         />
     })
 
 
-    if (deleteError | updateError | addError) { return <h1>Error happened "{error}"</h1> }
-    if (isDeleteLoading | isUpdateLoading | isAddLoading) { return <div className='loader-wrap'><Loader /></div> }
+    if (deleteError | updateError) { return <h1>Error happened "{deleteError | updateError}"</h1> }
+    if (isDeleteLoading | isUpdateLoading) { return <div className='loader-wrap'><Loader /></div> }
 
     return (
         <div className="table-container">
