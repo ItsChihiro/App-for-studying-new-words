4fcd417360c90561d629ab8f7c68298ef0dc13d9
Maryam <maryam@sosiv.io>
2024-03-07 21:47:53 +0200

    use mobx in TableWords

--- src/components/TableWords/TableWords.jsx
+++ src/components/TableWords/TableWords.jsx
@@ -1,46 +1,24 @@
+import { inject, observer } from 'mobx-react';
+import { useEffect } from 'react';
 import TableRow from '../TableRow/TableRow';
 import './TableWords.scss'
-import { MyContext } from '../../context/MyContext';
-import { useContext } from 'react';
-import { useFetching } from '../../hooks/useFetching';
-import Api from '../../services/Api';
 import Loader from '../UI/Loader/Loader';
 
-export default function TableWords() {
-    const { setDataServer, dataServer, getWordsServer } = useContext(MyContext)
 
-    //функция удаления слова
-    const [deleteWord, isDeleteLoading, deleteError] = useFetching(async (id) => {
-        await Api.deleteWord(id);
-        const wordsServer = getWordsServer();
-        setDataServer(wordsServer);
-    })
+function TableWords({ words, deleteWord, update, isLoading, error }) {
 
-
-    //функция обновления слова
-    const [updateWord, isUpdateLoading, updateError] = useFetching(async (id, data) => {
-        const updatedWord = await Api.updateWord(id, data)
-        setDataServer(dataServer.map((item) => {
-            if (item.id === id) {
-                return { ...item, ...updatedWord }
-            }
-            return item;
-        }))
-    })
-
-
-    const rows = dataServer.map((row) => {
+    const rows = words.map((row) => {
         return <TableRow
             {...row}
             key={row.id}
             deleteWord={deleteWord}
-            updateWord={updateWord}
+            updateWord={update}
         />
     })
 
-    if (deleteError) { return <h1>Error happened "{deleteError}"</h1> }
-    if (updateError) { return <h1>Error happened "{updateError}"</h1> }
-    if (isDeleteLoading | isUpdateLoading) { return <div className='loader-wrap'><Loader /></div> }
+
+    if (error) { return <h1>Error happened "{error}"</h1> }
+    if (isLoading) { return <div className='loader-wrap'><Loader /></div> }
 
     return (
         <div className="table-container">
@@ -60,5 +38,18 @@ export default function TableWords() {
             </table>
         </div>
     );
-}
+};
 
+
+
+
+export default inject(({ wordsStore }) => {
+    const { words, loadData, add, update, deleteWord, isLoading, error } = wordsStore;
+    // useEffect(() => {
+    //     loadData();
+    // }, [])
+
+    return {
+        words, loadData, add, update, deleteWord, isLoading, error
+    };
+})(observer(TableWords));
