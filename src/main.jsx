7ef190b8d303c07d296bd5382788ba7f4320cf78
Maryam <maryam@sosiv.io>
2024-03-07 21:47:02 +0200

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'mobx-react'
 import App from './components/App/App'
 import './style/index.scss'
-import { MyContextComponent } from './context/MyContext'
+import WordsStore from './store/WordsStore'
+
+export const stores = {
+  wordsStore: new WordsStore()
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MyContextComponent>
+    <Provider {...stores}>
       <App />
-    </MyContextComponent>
+    </Provider>
   </React.StrictMode>
 )
